f0dc0070a5d29f192063430702ca412c26f839c3
Alex Yong <alexjyong@gmail.com>
2022-07-20 15:49:41 -0400

    storing this file for future review

---
+++ data/plants.js
@@ -0,0 +1,97 @@
+ProbingPlantsGravity
+
+Very low
+They consist of huge webs of gossamer threads that reach for miles.
+They consist of a chaotic mound of rhizomes that regularly tumble over the surface.
+They rise far above the surface. Many plants seem to grow on top of each other.
+
+Low
+They look a lot like sponges on Earth, with a mind-boggling variety of colors and textures.
+They are usually long, thin shoots of weeds and their alien biology causes them to give off a lot of strange smells.
+Many of them actively orient themselves towards the sun of this planet and follow its course across the sky from dawn to dusk.
+
+Normal
+They are shaped like trees on Earth, in a wide variety of sizes, and their alien biochemistry makes them phosphorescent, so they glow for several hours after dusk.
+They look like vast fields of large flowers and appear to be use pollen to maintain some form of communication that is too complicated for the ship to decode.
+Most are bush-sized and very similar to each other. The probe discovers these bushes are connected by common roots, making them arguably parts of single very large organisms.
+
+
+High
+They look similar to cacti on Earth, with round, leafless shapes.
+They look like a prairie of fluorescent grasses and weeds and seem to grow and die very quickly.
+They create very tough structures that resemble coral reefs, living plants growing on top of their dead ancestors.
+
+
+Very high
+The plant life here is compressed into a thick crust of single-cell life forms that covers most of the surface of this planet.
+They are similar to mosses and lichens and seem to grow in complex fractal geometric patterns.
+They resemble a wide variety of small grasses and herbs. They seem to be slightly magnetic and their growth follows the magnetic field lines of the planet.
+
+
+
+ProbingPlantsTemperature
+
+Edible
+Very cold
+Their biology is based on fluid hydrocarbons, some of which can easily be converted into edible carbohydrates.
+They appear to be drawing heat and nutrients from very deep roots and some of the organic compounds they can produce in this way are edible by humans.
+
+Cold
+They use round crystalline structures to concentrate daylight into pockets of heat where they can grow, using chemistry similar enough to Earth's to be usable by humans.
+Completely frozen most of the time, they nevertheless grow in recognizable, and edible, organic processes during peak daylight hours.
+
+Normal
+They use a wide variety of proteins in their outer shells, some of which are edible for humans.
+Some of them happen to store energy in Earth-like fat molecules that make them a usable food source.
+
+Hot
+They withdraw into the ground during the day but the growth they do during nighttime is similar enough to Earth to produce some edible plant material.
+They store energy in sacs of liquid fats which could be cooled and filtered for human consumption.
+
+Very hot
+They use liquid metals for their metabolism and seem more machine-like than organic, but their waste products contain a wide range of compounds, including edible ones that could be filtered out.
+They use enormous crystalline structures to radiate heat away from their roots, where they have biological processes that include molecules humans could eat.
+
+Yellow (not edible, not poisonous)
+
+Very cold
+They appear to conserve energy by growing extremely slowly, over centuries or millenia.
+They seem to rely on flows of liquid oxygen and evaporate when exposed to Earth-like temperatures.
+
+Cold
+They have a strange fluorocarbon-based biology that works in the cold environment. This makes them glow in the dark and inedible to humans.
+They use large amounts of a strange form of photosynthesis to gather energy on this frigid world, but the highly complex chemicals they use to store it are not digestible by humans.
+
+Normal
+They are built out of complex arrangements of salt crystals, not nutritious for humans but not poisonous either.
+They are built out of polymers that on Earth would have been considered anorganic, completely inedible for humans.
+
+Hot
+Their biochemistry is based on sulfur, not carbon, and completely incompatible with human bodies.
+Their metabolism uses a lot of light metals like mercury and magnesium, making them incompatible with human biology.
+
+Very hot
+They use interactions of complex metallic crystals that the drone has nothing to compare them with, but will certainly not be edible.
+Their cell-like substructures are like furnaces of molten metals, utterly foreign to Earth-based biology.
+
+Poisonous
+
+Very cold
+They manage to survive in the extreme cold by maintaining a highly acidic, corrosive environment that would be very dangerous to humans.
+They use heat from radioactive decay to sustain life in the extreme cold and would cause radiation poisoning in any nearby humans.
+
+Cold
+They produce large amounts of ethylene glycol in clouds around themselves. This makes them capable of withstanding the cold, but also extremely dangerous to any nearby humans.
+They accumulate and emit heat in radioactive isotopes of heavy metals that are very dangerous to nearby humans.
+
+Normal
+They constantly surround themselves with clouds of pollen-like particles that would overwhelm a human immune system and are therefore very dangerous to humans.
+Their biochemical processes are extremely fast and energetic. While they are too complex for the drone to fully analyze, it is clear they are too volatile for human bodies to handle.
+
+Hot
+They survive the heat using an arsenic-based biochemistry and frequently burst into flames, creating fumes that are very poisonous to humans.
+Their shells and the pollen they emit are made of asbestos, making them extremely poisonous for humans.
+
+Very hot
+They breathe out aerosols of molten lead droplets that are highly poisonous to humans.
+They are very volatile and frequently burst into shrapnel of tiny jagged shards of metal that would be lethal if inhaled by a human.
